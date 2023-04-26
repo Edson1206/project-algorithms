0c141a169eddbc064b39e80b9409d013629e78c0
Edson Pereira <ed.1206@gmail.com>
2023-04-25 22:14:02 -0300

    feat: :sparkles: Implemented study schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students = 0
+    try:
+        for entry, exit in permanence_period:
+            if entry <= target_time <= exit:
+                students += 1
+        return students
+    except TypeError:
+        return None
